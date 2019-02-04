f893b269bd0ca0020d0dba715f16407b492de8d9
Francis Karagu <fkaragu@gmail.com>
2019-02-04 11:29:56 +0300

    Added link to bootstrap

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/francs/Desktop/core/back/watchlist/virtual"
+set -gx VIRTUAL_ENV "/home/francs/Desktop/core/back/NewApi/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
