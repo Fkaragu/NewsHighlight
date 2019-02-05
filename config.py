8ad4f061012a8dbfec4c42cfebb264ccc8d342b4
Francis Karagu <fkaragu@gmail.com>
2019-02-05 14:11:23 +0300

    Modified articles page

--- config.py
+++ config.py
@@ -6,16 +6,7 @@ class Config:
   '''
   NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?language=en&apiKey={}'
   NEWS_API_KEY = 'ec8d248df3204bd999e2821ee4bbee57'
-  NEWS_API_BASE_URL2 = 'https://newsapi.org/v2/top-headlines?language=en&apiKey={}'
-
-#   top-headlines?language=en
-#   NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?apiKey={}'
-#   NEWS_API_KEY = 'ec8d248df3204bd999e2821ee4bbee57'
-
-
-
-# https://newsapi.org/v2/sources?apiKey=ec8d248df3204bd999e2821ee4bbee57
-# https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=ec8d248df3204bd999e2821ee4bbee57
+  NEWS_API_BASE_URL2 = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize=100&apiKey={}'
 
 
 class ProdConfig(Config):
